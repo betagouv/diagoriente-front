89eb84a1b5378296799ad91ff68811d00e4179d3
Mahdi Hellal <mahdihellal@gmail.com>
2019-04-25 18:14:34 +0100

    home page

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from '../../components/buttons/RoundButton/RoundButton';
 import Circle from '../../components/ui/Circle/Circle';
-import Card from '../../components/cards/Card/Card';
-import WithSub from '../../components/ui/WithSub/WithSub';
-import './home.css';
-import HomeCard from '../../components/cards/HomeCard/HomeCard';
+/* import classNames from '../../../utils/classNames';
+*/ import classes from './home.module.scss';
+import Card from '../../components/ui/card/card';
+import WithSub from '../../components/ui/withsub/withSub';
+import HomeCard from '../../components/ui/homeCard/homeCard';
 
 const HomeContainer = ({ history }: any) => {
   const navigate = () => {
@@ -27,13 +28,31 @@ const HomeContainer = ({ history }: any) => {
   }
 
   return (
-    <div style={{ width: '100%' }}>
-      <div className={'container'}>{items1}</div>
-      <div style={{ marginTop: 20 }} className={'container'}>
-        {items2}
-      </div>
-      <div style={{ marginTop: 20 }} className={'container'}>
-        {items3}
+    <div className={classes.home}>
+      <WithSub  className={classes.WithSub} title={'Trouve ta voie !'} subTitle={'Réleve tes competences et choisis ton futur métier'}  />
+      <Button className={classes.commencerBtn}>Commencer</Button>
+      <div className={classes.CardContainer}>
+        <HomeCard
+          title=""
+          subTitle="Réponds à quelques questions sur tes passions, et joue à des jeux"
+          image="https://steemitimages.com/p/hgjbkodzNP3kumx8jDvG5UJWWNpEGQeXqQ3r48B8V6PwVj3oj4MLE4JUz5Fdxy8kEpZLox2JDyr5N2ki5XzdFgSFRv?format=match&mode=fit"
+          buttonText="commencer"
+          cardNumber={1}
+        />
+        <HomeCard
+          title=""
+          subTitle="Réponds à quelques questions sur tes passions, et joue à des jeux"
+          image="https://steemitimages.com/p/hgjbkodzNP3kumx8jDvG5UJWWNpEGQeXqQ3r48B8V6PwVj3oj4MLE4JUz5Fdxy8kEpZLox2JDyr5N2ki5XzdFgSFRv?format=match&mode=fit"
+          buttonText="commencer"
+          cardNumber={1}
+        />
+        <HomeCard
+          title=""
+          subTitle="Réponds à quelques questions sur tes passions, et joue à des jeux"
+          image="https://steemitimages.com/p/hgjbkodzNP3kumx8jDvG5UJWWNpEGQeXqQ3r48B8V6PwVj3oj4MLE4JUz5Fdxy8kEpZLox2JDyr5N2ki5XzdFgSFRv?format=match&mode=fit"
+          buttonText="commencer"
+          cardNumber={1}
+        />
       </div>
       <div style={{ marginTop: 20 }} className={'container'}>
         <div className={'item-4'} style={{ background: 'blue', height: 50 }} />
