2463b4e5f2411d11804f8a83afa9339cda799afc
Mahdi Hellal <mahdihellal@gmail.com>
2019-05-01 12:24:24 +0100

    shapes

---
+++ src/components/ui/shapes/questionMark/questionMark.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './question.module.scss';
+import classNames from '../../../../utils/classNames';
+
+const QuestionMarks = ({ className, children, ...other }: React.HTMLAttributes<HTMLElement>) => {
+  return (
+    <div className={classNames(classes.container, className)} {...other}>
+      <div className={classes.mark1} />
+      <div className={classes.mark2} />
+      <div className={classes.mark3} />
+    </div>
+  );
+};
+
+export default QuestionMarks;
