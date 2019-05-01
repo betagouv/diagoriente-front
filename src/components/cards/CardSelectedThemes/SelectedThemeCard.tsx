84c50e80d7fcf991e329427bd350b7d2b9553faa
Mahdi Hellal <mahdihellal@gmail.com>
2019-05-01 16:19:49 +0100

    update selected theme component

--- src/components/cards/CardSelectedThemes/SelectedThemeCard.tsx
+++ src/components/cards/CardSelectedThemes/SelectedThemeCard.tsx
@@ -5,14 +5,20 @@ interface IProps {
     title: string;
     logo?: string;
     open?: boolean;
+    isSelected?: boolean;
 }
 
-const SelectedCard = ({ title, logo, open }: IProps) => (
-    <div className={classes.container}>
-        {logo && <div className={classes.logo_container}>
-            <img alt='logo' src={logo} className={classes.logo} />
-        </div>}
-        <span className={classes.theme_title}>{title}</span>
+const SelectedCard = ({ title, logo, isSelected }: IProps) => (
+    <div className={classes.wrapper}>
+        <div className={isSelected ? classes.box_selected : classes.container}>
+            {logo && <div className={classes.logo_container}>
+                <img alt='logo' src={logo} className={classes.logo} />
+            </div>}
+            <span className={classes.theme_title}>{title}</span>
+
+        </div>
+        {isSelected && <div className={classes.white_div}><div className={classes.triangle} /></div>}
+
     </div>
 )
 export default SelectedCard;
