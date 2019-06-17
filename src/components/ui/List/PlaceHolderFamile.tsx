6d9d83a70883b4da9312d12d13b46f4e146353d7
Mahdi Hellal <mahdihellal@gmail.com>
2019-06-17 19:09:59 +0100

    update placeholder sidebar favoris

--- src/components/ui/List/PlaceHolderFamile.tsx
+++ src/components/ui/List/PlaceHolderFamile.tsx
@@ -9,10 +9,10 @@ interface IProps {
 const PlaceHolderFamile = ({ index }: IProps) => (
   <div className={classes.container}>
     <ContentLoader height={120} width={500} speed={3} primaryColor="#acabab" secondaryColor="#f7f7f7">
-      <rect x="116" y="37" rx="0" ry="0" width="200" height="7" />
-      <rect x="116" y="55" rx="0" ry="0" width="230" height="8" />
-      <rect x="117" y="75" rx="0" ry="0" width="170" height="8" />
-      <circle cx="57" cy="61" r="51" />
+      <rect x="140" y="37" rx="0" ry="0" width="200" height="7" />
+      <rect x="140" y="55" rx="0" ry="0" width="230" height="8" />
+      <rect x="145" y="75" rx="0" ry="0" width="170" height="8" />
+      <circle cx="60" cy="60" r="60" />
     </ContentLoader>
   </div>
 );
