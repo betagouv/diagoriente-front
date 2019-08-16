f0c3ab79f5d6112feac9ccd1890d8c6896c344f3
Mahdi Hellal <mahdihellal@gmail.com>
2019-08-16 17:06:40 +0100

    gif optimisation

--- src/sagas/listFamille.ts
+++ src/sagas/listFamille.ts
@@ -1,5 +1,5 @@
 import {
- takeLatest, call, all, put,
+ takeLatest, call, all, put, select,
 } from 'redux-saga/effects';
 
 import { IFamille } from 'reducers';
@@ -28,18 +28,41 @@ function* listFamilleRequest({ payload }: { type: string; payload: ListFamillePa
         data: list.data.data,
       }),
     );
+    const arrayList = [...list.data.data];
+    const data: any[][] = [];
+    const { length } = arrayList;
+    let arrayBythree: any = [];
+    arrayList.forEach((element: any, i) => {
+      if (i === length - 1 || arrayBythree.length === 2) {
+        data.push([...arrayBythree, element]);
+        arrayBythree = [];
+      } else {
+        arrayBythree.push(element);
+      }
+    });
+    console.log(data);
 
-    const promise = list.data.data.map(item => call(getFamilleDetails, item._id));
-    const results: Response<IFamille>[] = yield all(promise);
-    const error = results.find(response => response && response.code !== 200);
-    if (!error) {
-      yield put(
-        listFamilleActions.listResourcesSuccess({
-          data: results.map(response => ({ resources: [{}], ...response.data })),
-        }),
-      );
-    } else {
-      yield put(listFamilleActions.listFamilleFailure({ error: error.message }));
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(data[i]);
+      const promise = yield all(data[i].map(itemChild => call(getFamilleDetails, itemChild._id)));
+      const results: Response<IFamille>[] = yield all(promise as any);
+      const error = results.find(response => response && response.code !== 200);
+      if (!error) {
+        const state = yield select();
+        yield put(
+          listFamilleActions.listResourcesSuccess({
+            // eslint-disable-next-line
+            data: state.listFamille.data.map((item: any, index: number) => {
+              if (Math.floor(index / 3) === i) {
+                return { resources: [{}], ...results[index % 3].data };
+              }
+              return item;
+            }),
+          }),
+        );
+      } else {
+        yield put(listFamilleActions.listFamilleFailure({ error: error.message }));
+      }
     }
   } else {
     yield put(listFamilleActions.listFamilleFailure({ error: list.message }));
