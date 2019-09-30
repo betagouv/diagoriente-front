263c4fbc08a7e2bc3c95f410307caeab6957d824
Mahdi Hellal <mahdihellal@gmail.com>
2019-09-30 18:24:33 +0100

    job modal refonte complete

--- src/components/VerticalStepper/VerticalStepper.tsx
+++ src/components/VerticalStepper/VerticalStepper.tsx
@@ -1,24 +1,50 @@
 import React, { Fragment } from 'react';
 import { IFamille } from 'reducers';
+import MultiIcons from 'components_v3/icons/multiIcon/multiIcon';
 import classes from './VerticalStepper.module.scss';
-/* import classNames from '../../../../utils/classNames';
- */
+
 interface IProps {
   handleClick: (index: number) => void;
   DisplayedFamily: number;
   selectedFamilys?: IFamille[] | any;
   listItems?: IFamille[] | any;
+  forQuestions?: boolean;
 }
 
-const VerticalStepper = ({ handleClick, DisplayedFamily, listItems }: IProps) => (
+const VerticalStepper = ({
+ handleClick, DisplayedFamily, listItems, forQuestions,
+}: IProps) => (
   <Fragment>
-    {listItems.map((famille: IFamille, index: number) => (
-      <div
-        key={famille._id}
-        className={DisplayedFamily === index ? classes.stepperCIRCLE : classes.stepperCircle}
-        onClick={() => handleClick(index)}
-      />
-    ))}
+    {listItems.map((famille: IFamille, index: number) =>
+      (!forQuestions ? (
+        <div
+          key={famille._id}
+          className={DisplayedFamily === index ? classes.stepperCIRCLE : classes.stepperCircle}
+          onClick={() => handleClick(index)}
+        />
+      ) : (
+        <React.Fragment>
+          {DisplayedFamily === index ? (
+            <MultiIcons
+              width="15"
+              height="15"
+              type="validate"
+              onClick={() => handleClick(index)}
+              className={classes.btn_validate}
+              Iconcolor="#00cfff"
+            />
+          ) : (
+            <MultiIcons
+              width="15"
+              height="15"
+              type="remove"
+              onClick={() => handleClick(index)}
+              className={classes.btn_validate}
+              Iconcolor="#e55e68"
+            />
+          )}
+        </React.Fragment>
+      )))}
   </Fragment>
 );
 export default VerticalStepper;
--- src/components_v3/Questions/Questions.tsx
+++ src/components_v3/Questions/Questions.tsx
@@ -1,8 +1,11 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import MultiIcons from 'components_v3/icons/multiIcon/multiIcon';
 import { map } from 'lodash';
-import Button from 'components/buttons/RoundButton/RoundButton';
+import ValidButton from 'components/buttons/RoundButton/RoundButton';
+import Button from 'components_v3/button/button';
 import classes from './question.module.scss';
+import { Carousel } from 'react-responsive-carousel';
+import VerticalStepper from 'components/VerticalStepper/VerticalStepper';
 
 type responseProps = {
   questionJobId: string;
@@ -39,7 +42,9 @@ const Questions = ({
     if (questions) {
       let updatedArray = [];
       const newArray: any[] = [];
-      updatedArray = questions.filter((item: any) => item.response === true || item.response === false);
+      updatedArray = questions.filter(
+        (item: any) => item.response === true || item.response === false,
+      );
       updatedArray.map((item: any) =>
         newArray.push({ questionJobId: item._id, response: item.response }));
       onChangeQuestion(newArray);
@@ -97,8 +102,82 @@ const Questions = ({
 
   const onValidate = () => {
     answersJobs.call({ parcourId, jobId, responses: responseQuestion }, 't');
-    onCloseModal();
+   //  onCloseModal();
   };
+  const [DisplayedChild, changeDisplayedChild] = useState(0);
+
+  const items = map(questions, (item: QuestionType, i) => {
+    const { index, selected } = getSelected(
+      responseQuestion,
+      (r: any) => item._id === (r.questionJobId || r._id),
+    );
+    const nextFilters: responseProps[] = [...responseQuestion];
+    const onClick = (response: boolean) => {
+      if (selected) {
+        const question = nextFilters[index];
+        if (question.response === response) {
+          nextFilters.splice(index, 1);
+        } else {
+          nextFilters[index] = { ...question, response };
+        }
+      } else {
+        nextFilters.push({
+          questionJobId: item._id,
+          response,
+        });
+      }
+
+      onChangeQuestion(nextFilters);
+    };
+    const initialStateValidate: string = selected && responseQuestion[index].response ? '#ffba27' : '#666';
+    const initialStateRemoeve: string = selected && responseQuestion[index].response ? '#666' : '#e55d67';
+
+    return (
+      <div key={item._id} className={classes.question}>
+        <span>{i + 1}/{questions.length}</span>
+        <div className={classes.question_title}>{item.label}</div>
+
+        <div className={classes.btn_container}>
+          {/* <MultiIcons
+            width="23"
+            height="23"
+            type="validate"
+            onClick={() => onClick(true)}
+            className={classes.btn_validate}
+            Iconcolor={selected ? initialStateValidate : '#7a93bc'}
+          /> */}
+          <Button
+            title="Oui"
+            color="blue"
+            onClick={() => {
+               onClick(true);
+              changeDisplayedChild(DisplayedChild + 1);
+              onValidate();
+              }}
+            className={classes.yesNoButton}
+          />
+          <Button
+            title="Non"
+            color="red"
+            onClick={() => {
+              onClick(false)
+              changeDisplayedChild(DisplayedChild + 1);
+              onValidate();
+            }}
+            className={classes.yesNoButton}
+          />
+          {/* <MultiIcons
+            width="23"
+            height="23"
+            type="remove"
+            onClick={() => onClick(false)}
+            className={classes.btn_remove}
+            Iconcolor={selected ? initialStateRemoeve : '#7a93bc'}
+          /> */}
+        </div>
+      </div>
+    );
+  })
   return (
     <Fragment>
       <div className={classes.title_container}>
@@ -111,65 +190,36 @@ const Questions = ({
       </div>
       <div className={classes.container} id="element">
         <div className={classes.question_containers}>
-          {map(questions, (item: QuestionType) => {
-            const { index, selected } = getSelected(
-              responseQuestion,
-              (r: any) => item._id === (r.questionJobId || r._id),
-            );
-            const nextFilters: responseProps[] = [...responseQuestion];
-            const onClick = (response: boolean) => {
-              if (selected) {
-                const question = nextFilters[index];
-                if (question.response === response) {
-                  nextFilters.splice(index, 1);
-                } else {
-                  nextFilters[index] = { ...question, response };
-                }
-              } else {
-                nextFilters.push({
-                  questionJobId: item._id,
-                  response,
-                });
-              }
-
-              onChangeQuestion(nextFilters);
-            };
-            const initialStateValidate: string = selected && responseQuestion[index].response ? '#ffba27' : '#666';
-            const initialStateRemoeve: string = selected && responseQuestion[index].response ? '#666' : '#e55d67';
-
-            return (
-              <div key={item._id} className={classes.question}>
-                <div className={classes.question_title}>{item.label}</div>
-
-                <div className={classes.btn_container}>
-                  <MultiIcons
-                    width="23"
-                    height="23"
-                    type="validate"
-                    onClick={() => onClick(true)}
-                    className={classes.btn_validate}
-                    Iconcolor={selected ? initialStateValidate : '#7a93bc'}
-                  />
-                  <MultiIcons
-                    width="23"
-                    height="23"
-                    type="remove"
-                    onClick={() => onClick(false)}
-                    className={classes.btn_remove}
-                    Iconcolor={selected ? initialStateRemoeve : '#7a93bc'}
-                  />
-                </div>
-              </div>
-            );
-          })}
+          <Carousel
+            showThumbs={false}
+            showIndicators={false}
+            showStatus={false}
+            width="500px"
+            stopOnHover={false}
+            onChange={index => changeDisplayedChild(index)}
+            useKeyboardArrows
+            showArrows={false}
+            selectedItem={DisplayedChild}
+            className={classes.carou}
+          >
+            {items}
+          </Carousel>
         </div>
-        {questions && questions.length !== 0 && (
+        {/* questions && questions.length !== 0 && (
           <div className={classes.container_btn}>
-            <Button onClick={onValidate} className={classes.btnValidate}>
+            <ValidButton onClick={onValidate} className={classes.btnValidate}>
               Valider
-            </Button>
+            </ValidButton>
           </div>
-        )}
+        ) */}
+        <div className={classes.dotsContainer}>
+          <VerticalStepper
+            handleClick={changeDisplayedChild}
+            DisplayedFamily={DisplayedChild}
+            listItems={items}
+            forQuestions
+          />
+        </div>
       </div>
     </Fragment>
   );
