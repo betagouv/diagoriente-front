fb5fa30f03317a8eae4542a0e5567625661e1cf0
Mahdi Hellal <mahdihellal@gmail.com>
2019-10-29 11:05:22 +0100

    fix type niveau job ts

--- src/requests/jobs.ts
+++ src/requests/jobs.ts
@@ -42,9 +42,10 @@ export const getMyJob = (
   accessibility?: string,
   algoType: 'interest_family' | 'family' | 'interest' = 'interest_family',
 ): Promise<Response<IJob[]>> => {
-  console.log('herrrree test', accessibility)
-  const env = environments && JSON.parse(environments).length ? environments : null;
+   const env = environments && JSON.parse(environments).length ? environments : null;
   const sect = secteur && JSON.parse(secteur).length ? secteur : null;
+  const niv = accessibility && JSON.parse(accessibility).length ? accessibility : null;
+
   return axiosGet('v1/jobs/myJobs', {
     params: {
       parcourId,
